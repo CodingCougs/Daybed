8940a37148628f5ca78f07e44dd11b63451d1ba8
Randy Tran <10tranr@gmail.com>
2017-11-15 12:41:23 -0800

    Added a footer and updated example content

--- src/root/root-App.js
+++ src/root/root-App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 
-import Navbar from '../components/navbar/navbar'
-
-
 export default ({ children }) => {
   return (
     <div className={'full'}>
-      <Navbar />
       {children}
     </div>
   );
